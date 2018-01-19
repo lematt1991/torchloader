6af7dc73d960bb808376d9bb9bc978ac5ec27d5f
Matt Le <ml9951@rit.edu>
2018-01-19 14:58:15 -0500

    Restructure for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='torchloader',
+    packages=['torchloader'],
+    version='0.1',
+    description='A PyTorch style dataloader that can be used anywhere',
+    author='Matt Le',
+    author_email='lematt1991@gmail.com',
+    url='https://github.com/ml9951/DataLoader',
+    download_url='https://github.com/ml9951/DataLoader/archive/master.zip',
+    keywords=['dataloader', 'pytorch', 'torch', 'multiprocess'],
+    classifiers=[]
+)
